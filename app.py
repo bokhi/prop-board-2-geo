30fbf7ae477cdc1efe8e246d20bcb96ff2af300c
Martin Boissier <martin.boissier@gmail.com>
2023-08-27 19:01:31 +0800

    aider: Added a dummy function to process the text listing and return structured data, and hooked up the map display component to show a Folium map centered at the location returned by the function.
    # Aider chat conversation:
    
    USER: ok know let's add a dummy function that would use the text listing and return structure data, like a dictionary with location lat,long and price that could then be passed to the st follium map component.
    create that function and return dummy value for now.
    then let's hook the map display component, base it on the snippet I gave above
    
    ASSISTANT: Sure, let's start by creating a dummy function that processes the text listing and returns structured data. We'll add this function to the `app.py` file.
    
    ```python
    app.py
    <<<<<<< HEAD
    import streamlit as st
    
    def main():
        st.title('Property Listing Processor')
        listing = st.text_area('Paste your property listing here:', '')
        if st.button('Process'):
            st.write('Processing...')
    
    if __name__ == "__main__":
        main()
    =======
    import streamlit as st
    
    def process_listing(listing):
        # This is a dummy function for now
        return {
            'location': {
                'lat': 51.5074,
                'long': -0.1278
            },
            'price': 1000000
        }
    
    def main():
        st.title('Property Listing Processor')
        listing = st.text_area('Paste your property listing here:', '')
        if st.button('Process'):
            result = process_listing(listing)
            st.write('Processing...')
            st.write(result)
    
    if __name__ == "__main__":
        main()
    >>>>>>> updated
    ```
    
    Next, let's hook up the map display component. We'll use the `streamlit_folium` package to display a Folium map in our Streamlit app. We'll add this to the `app.py` file as well.
    
    ```python
    app.py
    <<<<<<< HEAD
    import streamlit as st
    
    def process_listing(listing):
        # This is a dummy function for now
        return {
            'location': {
                'lat': 51.5074,
                'long': -0.1278
            },
            'price': 1000000
        }
    
    def main():
        st.title('Property Listing Processor')
        listing = st.text_area('Paste your property listing here:', '')
        if st.button('Process'):
            result = process_listing(listing)
            st.write('Processing...')
            st.write(result)
    
    if __name__ == "__main__":
        main()
    =======
    import streamlit as st
    import folium
    from streamlit_folium import folium_static
    
    def process_listing(listing):
        # This is a dummy function for now
        return {
            'location': {
                'lat': 51.5074,
                'long': -0.1278
            },
            'price': 1000000
        }
    
    def main():
        st.title('Property Listing Processor')
        listing = st.text_area('Paste your property listing here:', '')
        if st.button('Process'):
            result = process_listing(listing)
            st.write('Processing...')
            st.write(result)
    
            # Create a map centered at the location
            m = folium.Map(location=[result['location']['lat'], result['location']['long']])
    
            # Display the map
            folium_static(m)
    
    if __name__ == "__main__":
        main()
    >>>>>>> updated
    ```
    
    These changes will add a map to your Streamlit app that is centered at the location returned by the `process_listing` function. For now, the function returns a fixed location (latitude 51.5074, longitude -0.1278), but you can update this function to process the listing and extract the actual location.

--- app.py
+++ app.py
@@ -1,10 +1,30 @@
 import streamlit as st
+import folium
+from streamlit_folium import folium_static
+
+def process_listing(listing):
+    # This is a dummy function for now
+    return {
+        'location': {
+            'lat': 51.5074,
+            'long': -0.1278
+        },
+        'price': 1000000
+    }
 
 def main():
     st.title('Property Listing Processor')
     listing = st.text_area('Paste your property listing here:', '')
     if st.button('Process'):
+        result = process_listing(listing)
         st.write('Processing...')
+        st.write(result)
+
+        # Create a map centered at the location
+        m = folium.Map(location=[result['location']['lat'], result['location']['long']])
+
+        # Display the map
+        folium_static(m)
 
 if __name__ == "__main__":
     main()
